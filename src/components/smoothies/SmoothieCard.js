772b5ed1a0a3141566979b30f93c7729f942e1ae
Evelyn <evelyn.chavez92@yahoo.com>
2022-01-21 13:41:54 -0600

    add PO

--- src/components/smoothies/SmoothieCard.js
+++ src/components/smoothies/SmoothieCard.js
@@ -3,7 +3,7 @@ export const SmoothieCard = ({smoothie}) => {
     return (
         <div className="smoothie-card">
     <h3>{smoothie.name}</h3>
-    <img src={window.location.origin+smoothie.url}/>
+    <img src={window.location.origin+smoothie.image}/>
     <h4> {smoothie.type}</h4>
             
         </div>
--- src/components/smoothies/SmoothieForm.js
+++ src/components/smoothies/SmoothieForm.js
@@ -1,25 +1,46 @@
 import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 export const SmoothieForm = () => {
+    const history = useHistory()
     const [type, setType] = useState("");
-    const [name, setname] = useState("");
-    const [image, setimage] = useState("");
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
     const [ingredients, setIngredients] = useState([]);
 
     const handleSubmit = e => {
         e.preventDefault()
+      if  ([type, name, image, ingredients].some(val => val.trim() === "")){
+             alert("Please fill out all the fields, thank you!!")
+             return null
+        }
+        const newSmoothies = {type, name, image, ingredients}
+
+        fetch("http://localhost:3001/smoothies", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newSmoothies)
+        })
+        setType("")
+        setName("")
+        setImage("")
+        setIngredients("")
+        history.push("/smoothies")
     }
     
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <lable htmlFor="type">Type</lable>
-                <input type ="text" name="type" id="type"/>
-                <lable htmlFor="name">Name</lable>
-                <input type ="text" name="name" id="name"/>
-                <lable htmlFor="image">Image Url</lable>
-                <input type ="text" name="image" id="image"/>
-                <lable htmlFor="ingredients">Ingredients</lable>
-                <input type ="text" name="ingredients" id="ingredients"/>
+                <label htmlFor="type">Type</label>
+                <input onChange={e => setType(e.target.value)} type ="text" name="type" id="type" value={type} required/> <br /><br />
+                <label htmlFor="name">Name</label>
+                <input onChange={e => setName(e.target.value)} type ="text" name="name" id="name" value={name} required/>  <br /><br />
+                <label htmlFor="image">Image Url</label>
+                <input onChange={e => setImage(e.target.value)} type ="text" name="image" id="image" value={image} required/> <br /><br />
+                <label htmlFor="ingredients">Ingredients</label>
+                <input onChange={e => setIngredients(e.target.value)} type ="text" name="ingredients" id="ingredients" value={ingredients} />  <br /><br />
+                <input type="submit" value="Create"/>
             </form>
         </div>
     )
