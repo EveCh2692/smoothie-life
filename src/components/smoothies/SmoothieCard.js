75cbfaa9625e5f7046b37e43291f92a17aea6ab9
Evelyn <evelyn.chavez92@yahoo.com>
2022-01-21 16:26:07 -0600

    try to make card flip

--- src/components/smoothies/SmoothieCard.js
+++ src/components/smoothies/SmoothieCard.js
@@ -5,9 +5,12 @@ import { useState, useEffect } from "react"
 export const SmoothieCard = ({smoothie}) => {
     const [clickFlip, setClickFlip] = useState(false);
     return (
-        <div className="smoothie-card">
+        <div className="smoothie-card"> 
+        
     <h3>{smoothie.name}</h3>
-    <img src={window.location.origin+smoothie.image} />
+    <img src={window.location.origin+smoothie.image}
+    onClick={() => setClickFlip(bool => !bool)}
+    />
     <h4> {smoothie.type}</h4>
     <h5>{smoothie.ingredients}</h5>
     
