dcaff3f0326a9325901acc34d84f1bd6f90592f1
Evelyn <evelyn.chavez92@yahoo.com>
2022-01-24 12:08:41 -0600

    Add function clickFlip to SmoothieCard component to make card flip

--- src/components/smoothies/SmoothieCard.js
+++ src/components/smoothies/SmoothieCard.js
@@ -8,11 +8,12 @@ export const SmoothieCard = ({smoothie}) => {
         <div className="smoothie-card"> 
         
     <h3>{smoothie.name}</h3>
-    <img src={window.location.origin+smoothie.image}
+    <img
+    src={window.location.origin+smoothie.image}
     onClick={() => setClickFlip(bool => !bool)}
     />
-    <h4> {smoothie.type}</h4>
-    <h5>{smoothie.ingredients}</h5>
+    {!clickFlip? <h4> {smoothie.type}</h4> :
+    <h5>{smoothie.ingredients}</h5>}
     
             
         </div>
