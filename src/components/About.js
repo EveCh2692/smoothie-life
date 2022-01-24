4fc5c6e4f8b0e05abe6f96a20dafcf95a14259d3
Evelyn <evelyn.chavez92@yahoo.com>
2022-01-24 14:52:36 -0600

    added break to each line in about

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,8 @@ export const About = () => {
              be difficult to even get a meal in throughout the day. That is where a smoothie can come in handy. You can drink your daily dose of fruits or veggies <br/>
              in a cup and sometimes you can even drink it as a meal. In it, I have shared six delicious and nutritious smoothies along with the type of smoothie they <br/>
              are. This application also allows you to create a recipe of their own and save it with the rest of the smoothies in hope to inspire others to drink their <br/>
-             daily dose of vitamins via a smoothie. I believe that a healthy lifestyle is essential to our physical and mental well-being, because what we put in our body also affects our brain/gut and how it functions. A simple change or addition as drinking a smoothie a day can shift the way you feel throughout the day.</p>
+             daily dose of vitamins via a smoothie. I believe that a healthy lifestyle is essential to our physical and mental well-being, because what we put in our body <br/>
+             also affects our brain/gut and how it functions. A simple change or addition as drinking a smoothie a day can shift the way you feel throughout the day.</p>
         </div>
     )
 }
